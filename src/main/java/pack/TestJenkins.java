607cb27e3fc88cf81b0b7a10d7fe4c212f82f4ff
thejeshwar06@gmail.com <thejeshwar06@gmail.com>
2019-11-11 14:51:13 +0530

    First Commit

---
+++ src/main/java/pack/TestJenkins.java
@@ -0,0 +1,14 @@
+package pack;
+
+import java.time.LocalDateTime;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+	@Test
+	public void justchecking() {
+		
+		System.out.println(LocalDateTime.now().toString()+" Okaaaay!!");
+	}
+
+}
